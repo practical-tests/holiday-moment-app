1f834332d6ec998fa0989e4f2cb5d24bde5302de
Léo Belini <leobelini96@gmail.com>
2023-01-11 21:50:37 -0300

    feat: create demo page

---
+++ src/routes/home/Home.tsx
@@ -0,0 +1,11 @@
+import { View, Button, Text } from 'native-base'
+import { env } from '../../utils'
+
+const Home = () => {
+  return <View >
+    <Button onPress={() => console.log("hello world")}>Click Me</Button>
+    <Text>{env.BASE_URL_API}</Text>
+  </View>
+}
+
+export default Home
