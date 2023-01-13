4f89fdcf75ed765fbf3318eec7b83e5b7ba97d65
Léo Belini <leobelini96@gmail.com>
2023-01-12 22:25:31 -0300

    feat: remove holiday action

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,37 +1,110 @@
-import useSWR from "swr";
-import { useContext } from "react";
-import { Center, FlatList } from "native-base";
+import { useContext, useState, useCallback, useEffect } from "react";
+import {
+  Box,
+  Center,
+  Divider,
+  FlatList,
+  Menu,
+  Pressable,
+  Text,
+} from "native-base";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 import { FeedItem } from "./FeedItem";
 import { AppContext } from "../../context";
 import { Loading } from "../../components/Loading";
+import { Holiday } from "../../types";
 
 const Home = () => {
   const { holidayDb } = useContext(AppContext);
-  const { isLoading, data } = useSWR(
-    "holidayDbKeys",
-    async () => {
-      const data = await holidayDb.getAll();
-      return data.sort(
-        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
-      );
-    },
-    { refreshInterval: 0 }
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<Holiday[]>([]);
 
-  if (isLoading)
-    return (
-      <Center height="full">
-        <Loading />
-      </Center>
+  const fetchHolidays = useCallback(async () => {
+    setIsLoading(true);
+    const data = await holidayDb.getAll();
+    setData(
+      data.sort(
+        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+      )
     );
+    setIsLoading(false);
+  }, []);
+
+  const removeHoliday = useCallback(async (holiday: Holiday) => {
+    setIsLoading(true);
+    await holidayDb.remove(holiday.id);
+    await fetchHolidays();
+  }, []);
+
+  useEffect(() => {
+    fetchHolidays();
+  }, []);
 
   return (
     <>
+      {isLoading && (
+        <>
+          <Box
+            backgroundColor={"gray.900"}
+            style={{
+              opacity: 0.5,
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              zIndex: 5,
+            }}
+          ></Box>
+          <Center
+            style={{
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              zIndex: 10,
+            }}
+          >
+            <Loading />
+          </Center>
+        </>
+      )}
       <FlatList
         data={data || []}
         keyExtractor={(item) => item.id}
-        renderItem={(item) => <FeedItem key={item.item.id} item={item.item} />}
+        renderItem={(item) => (
+          <FeedItem
+            key={item.item.id}
+            item={item.item}
+            menu={
+              <Menu
+                trigger={(triggerProps) => {
+                  return (
+                    <Pressable
+                      accessibilityLabel="More options menu"
+                      {...triggerProps}
+                    >
+                      <MaterialCommunityIcons
+                        name="dots-vertical"
+                        size={20}
+                        color="#73757d"
+                      />
+                    </Pressable>
+                  );
+                }}
+              >
+                <Menu.Item>Editar</Menu.Item>
+                <Menu.Item>Registar momento</Menu.Item>
+                <Divider />
+                <Menu.Item onPress={() => removeHoliday(item.item)}>
+                  <Text color="red.500">Remover</Text>
+                </Menu.Item>
+              </Menu>
+            }
+          />
+        )}
       />
 
       {/* <Box width={"full"} borderRadius="md" backgroundColor="lightBlue.50">
