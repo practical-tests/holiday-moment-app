facbc3d6937499d8700323c2eb3f749fb6a93388
Léo Belini <leobelini96@gmail.com>
2023-01-13 15:41:34 -0300

    feat: create usePromise hook

---
+++ src/hooks/usePromise.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect, useCallback } from "react";
+
+type Config = {
+  callOnStart?: boolean;
+};
+
+const usePromise = <Data = any, Error = any>(
+  fnPromise: () => Data | Promise<Data>,
+  config?: Config
+) => {
+  const [isLoading, setLoading] = useState(false);
+  const [returnError, setReturnError] = useState<Error | undefined>(undefined);
+  const [returnData, setReturnData] = useState<Data | undefined>(undefined);
+
+  const callPromise = useCallback(async () => {
+    try {
+      setLoading(true);
+      const data = await fnPromise();
+      setReturnData(data);
+    } catch (error) {
+      setReturnError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (config?.callOnStart) callPromise();
+  }, []);
+
+  return {
+    loading: { setIsLoading: setLoading, isLoading },
+    error: returnError,
+    data: returnData,
+    call: callPromise,
+  };
+};
+
+export { usePromise };
