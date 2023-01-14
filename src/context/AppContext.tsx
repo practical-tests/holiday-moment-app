ddeb7fdf20d379b836b842bb0f7ad8083855b411
Léo Belini <leobelini96@gmail.com>
2023-01-14 15:08:55 -0300

    feat: create AppBar

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,6 +1,7 @@
-import { useCallback } from "react";
-import { Center } from "native-base";
-import { createContext, useMemo } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useCallback, useState, createContext, useMemo } from "react";
+import { Box, Center, Flex, HStack, Pressable, Text } from "native-base";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 import { API } from "../api";
 import { Holiday } from "../types";
@@ -11,6 +12,7 @@ import { usePromise } from "../hooks";
 
 interface TypeContext {
   holidayDb: Storage<Holiday>;
+  setTitle: (title: string) => void;
 }
 
 export const AppContext = createContext<TypeContext>(null!);
@@ -22,6 +24,9 @@ type AppContextProviderProps = {
 const AppContextProvider: React.FC<AppContextProviderProps> = ({
   children,
 }) => {
+  const navigation = useNavigation();
+
+  const [title, setTitle] = useState("Feed");
   const holidayDb = useMemo(() => new Storage<Holiday>(`holidays`), []);
 
   const fetchHolidays = useCallback(async () => {
@@ -46,8 +51,40 @@ const AppContextProvider: React.FC<AppContextProviderProps> = ({
     <AppContext.Provider
       value={{
         holidayDb,
+        setTitle,
       }}
     >
+      <HStack
+        bg="#117cfd"
+        px="1"
+        pb="2"
+        justifyContent="space-between"
+        alignItems="center"
+        w="100%"
+      >
+        <Box w="10">
+          {navigation.canGoBack() && (
+            <Pressable
+              w="full"
+              alignItems="center"
+              justifyContent="center"
+              onPress={() => navigation.goBack()}
+            >
+              <MaterialCommunityIcons
+                name="arrow-left"
+                size={20}
+                color="white"
+              />
+            </Pressable>
+          )}
+        </Box>
+        <Flex alignItems="center" justifyContent="center" flex="1">
+          <Text color="white" fontSize="20" fontFamily="Poppins_700Bold">
+            {title}
+          </Text>
+        </Flex>
+        <Box w="10" />
+      </HStack>
       {children}
     </AppContext.Provider>
   );
