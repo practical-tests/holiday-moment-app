2f616224b762766095c2917a4a8b71bb967adaa6
Léo Belini <leobelini96@gmail.com>
2023-01-14 15:08:28 -0300

    feat: add google fonts poppins

--- App.tsx
+++ App.tsx
@@ -2,17 +2,28 @@ import "react-native-get-random-values";
 
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 import { Routes } from "./src/routes";
 import { AppContextProvider } from "./src/context";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_700Bold });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <NativeBaseProvider>
         <AppContextProvider>
           <Routes />
-          <StatusBar />
+          <StatusBar backgroundColor="#117cfd" />
         </AppContextProvider>
       </NativeBaseProvider>
     </NavigationContainer>
